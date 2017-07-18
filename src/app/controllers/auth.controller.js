e4ad66e1c56e9435fdad32e8a7df975ebf74a83c
brandonfujii <brandonfujii@tumblr.com>
2017-07-18 00:25:39 -0400

    auth comments

--- src/app/controllers/auth.controller.js
+++ src/app/controllers/auth.controller.js
@@ -7,7 +7,9 @@ import jwt from 'jsonwebtoken';
 import models from '../models';
 import tokenController from './token.controller';
 import { isEmail, to } from '../utils';
-
+import {
+    LoginError
+} from '../errors';
 
 /** 
  * Creates and returns a user instance based on given options
@@ -16,7 +18,9 @@ import { isEmail, to } from '../utils';
  * @param {String|undefined}  options.lastName 
  * @param {String}            options.email      
  * @param {String}            options.password    
- * @param {"admin"|"user"}    options.privilege   
+ * @param {"admin"|"user"}    options.privilege 
+ * @returns {Promise<User, Error>} - returns a newly created
+ * user if resolved; otherwise, throws an error  
  */
 const createUser = async function(options: Object): Promise<models.User> {
     let {
@@ -43,10 +47,17 @@ const createUser = async function(options: Object): Promise<models.User> {
     });
 };
 
+/** 
+ * Creates and returns a user instance based on given options
+ * @param {User}                   user - a user that needs to be authenticated
+ * @param {String}    candidatePassword - a password to match
+ * @returns {Promise<User, LoginError>} - a promise that returns
+ * a verified user when resolved; otherwise rejects a login error
+ */
 const verifyUser = async function(user: models.User, candidatePassword: string): Promise<models.User> {
     return new Promise(async (resolve, reject) => {
         if (!user) {
-            resolve(new Error('Login failed!'));
+            reject(new LoginError());
         }
 
         const { 
@@ -55,17 +66,24 @@ const verifyUser = async function(user: models.User, candidatePassword: string):
         } = await to(user.verifyPassword(candidatePassword));
 
         if (err != null) {
-            resolve(err);
+            reject(err);
         }
 
         if (!isAuthenticated) {
-            resolve(new Error('Login failed'));
+            reject(new LoginError());
         }
 
         resolve(user);
     });
 }
 
+/** 
+ * Creates an authorization token based on a secret and 
+ * user information
+ * @param   {User}              user - a user that needs to be authenticated
+ * @returns {Promise<String, Error>} - a promise that returns a signed 
+ * JSON Web Token when resolved; otherwise rejects an error
+ */
 const signToken = async function(user: models.User): Promise<string> {
     const auth = config.get('auth');
     const {
@@ -102,6 +120,15 @@ type TokenPairType = {
     token: models.Token
 };
 
+/** 
+ * Creates a Token instance based on a given auth token and
+ * updates its corresponding owner
+ * @param   {User}                     user - a user that needs to be authenticated
+ * @param   {String}                  token - a JSON web token
+ * @returns {Promise<TokenPairType, Error>} - a promise that returns an object
+ * that contains a User-Token instance pair when resolved; otherwise rejects an error
+ * and rollbacks transaction
+ */
 const createToken = async function(user: models.User, token: string): Promise<TokenPairType> {
     return new Promise(async (resolve, reject) => {
         const t = await models.sequelize.transaction();
@@ -131,6 +158,13 @@ const createToken = async function(user: models.User, token: string): Promise<To
     });
 }
 
+/** 
+ * Checks if a user has any existing authorization tokens. If not,
+ * creates a token
+ * @param   {User}                     user - a user that needs to be authenticated
+ * @returns {Promise<TokenPairType, Error>} - a promise that returns an object
+ * that contains a User-Token instance pair when resolved; otherwise rejects an error
+ */
 const checkToken = async function(user: models.User): Promise<TokenPairType> {
     return new Promise(async (resolve, reject) => {
         let {
