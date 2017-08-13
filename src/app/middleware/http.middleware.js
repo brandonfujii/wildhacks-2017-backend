676d75369b0ec711f014b4f9c280c692e435ecbb
brandonfujii <brandonfujii2018@u.northwestern.edu>
2017-08-13 14:57:19 -0400

    allow options to request accepted headers from fetch api

--- src/app/middleware/http.middleware.js
+++ src/app/middleware/http.middleware.js
@@ -3,7 +3,7 @@
 const httpMiddleware = (req: $Request, res: $Response, next: express$NextFunction) => {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE, OPTIONS');
-    res.header('Access-Control-Allow-Headers', 'Authorization, Accept, X-Access-Token, X-Access-Gatekey, X-Key, Content-Type, Content-Length');
+    res.header('Access-Control-Allow-Headers', 'Accept, X-Access-Token, X-Access-Gatekey, X-Key, Content-Type, Content-Length');
     next();
 }
 
