c4d752229f0d97620bc699bebaf2ef6f2a4415ef
brandonfujii <brandonfujii2018@u.northwestern.edu>
2017-09-12 01:07:06 -0500

    updated events, users, teams in public api

--- src/app/controllers/public.controller.js
+++ src/app/controllers/public.controller.js
@@ -3,17 +3,27 @@
 import models from '../models';
 import userController from '../controllers/user.controller';
 
-const USER_ATTRIBUTE_BLACKLIST = ['email', 'password', 'privilege', 'token_id', 'application_id'];
+const USER_ATTRIBUTE_BLACKLIST = ['email', 'password', 'privilege', 'token_id', 'application_id', 'verification_token_id', 'is_verified'];
 const APPLICATION_ATTRIBUTE_BLACKLIST = ['id', 'first_name', 'last_name', 'personal_website', 'github_username', 'rsvp', 'decision', 'application_status', 'user_id', 'resume_id'];
 const SKILL_ATTRIBUTE_BLACKLIST = ['id', 'meta_value', 'created_at', 'updated_at', 'ApplicationSkill'];
 const TALK_ATTRIBUTE_BLACKLIST = ['id', 'speaker_id'];
 const TEAM_ATTRIBUTE_BLACKLIST = ['id'];
 const EVENT_ATTRIBUTE_BLACKLIST = ['id', 'meta_value', 'created_at', 'updated_at'];
 
-const getUsers = async function(limit: number = 10, offset: number): Promise<Array<models.User>> {
+const getUsersWithLimit = async function(limit: number, offset: number, options: object): Promise<Array<models.User>> {
     return models.User.findAll({
         limit,
         offset,
+        ...options,
+    });
+};
+
+const getAllUsers = async function(options: object): Promise<Array<models.User>> {
+    return models.User.findAll(options);
+};
+
+const getUsers = async function(pageNumber: number = 1, limit: ?number): Promise<Object> {
+    const options = {
         attributes: {
             exclude: USER_ATTRIBUTE_BLACKLIST,
         },
@@ -53,39 +63,155 @@ const getUsers = async function(limit: number = 10, offset: number): Promise<Arr
                 }
             },
         ],
-    });
-};
-
-/**
- * Gets a page of users based on a page number and page limit
- * @param   {number}    pageNumber - the # that corresponds to a subset of Users
- * @param   {number}         limit - the # of users per page
- * @returns {Promise<Object>} - returns Promise containing an
- * array of User instances, page number, page size, and number of total users
- */
-const getUserPage = async function(pageNumber: number = 1, limit: number = 10): Promise<Object> {
-    const offset = pageNumber < 1 ? 0 : (pageNumber - 1) * limit; // zero-index page number
+    };
 
     return new Promise(async (resolve, reject) => {
         try {
-            const [users, count] = await Promise.all([
-                getUsers(limit, offset),
-                userController.getUserCount(),
-            ]);
+            if (limit) {
+                const offset = pageNumber < 1 ? 0 : (pageNumber - 1) * limit;          
+                const [users, count] = await Promise.all([
+                    getUsersWithLimit(limit, offset, options),
+                    userController.getUserCount(),
+                ]);
 
-            resolve({
-                page: pageNumber,
-                pageSize: limit,
-                totalPages: Math.ceil(count / limit),
-                totalUsers: count,
-                users: users ? users : [],
-            });
+                resolve({
+                    page: pageNumber,
+                    pageSize: limit,
+                    totalPages: Math.ceil(count / limit),
+                    count,
+                    users,
+                });
+            } else {
+                const users = await getAllUsers(options);
+
+                resolve({
+                    count: users.length,
+                    users,
+                });
+            }
         } catch(err) {
             reject(err);
         }
     });
 };
 
+const getTeamsWithLimit = async function(limit: number, offset: number, options: object): Promise<Array<models.Team>> {
+    return models.Team.findAll({
+        limit,
+        offset,
+        ...options,
+    });
+};
+
+const getAllTeams = async function(options: object): Promise<Array<models.Team>> {
+    return models.Team.findAll(options);
+};
+
+const getTeams = async function(pageNumber: number = 1, limit: ?number): Promise<Object> {
+    const options = {
+        attributes: {
+            exclude: TEAM_ATTRIBUTE_BLACKLIST,   
+        },
+        include: [
+            {
+                model: models.User,
+                include: [
+                    {
+                        model: models.Application,
+                        attributes: {
+                            exclude: APPLICATION_ATTRIBUTE_BLACKLIST,
+                        },
+                    },
+                ],
+                attributes: {
+                    exclude: USER_ATTRIBUTE_BLACKLIST,
+                }
+            }
+        ],
+    };
+
+    return new Promise(async (resolve, reject) => {
+        try {
+            if (limit) {
+                const offset = pageNumber < 1 ? 0 : (pageNumber - 1) * limit;            
+                const [teams, count] = await Promise.all([
+                    getTeamsWithLimit(limit, offset, options),
+                    models.Team.count(),
+                ]);
+
+                resolve({
+                    page: pageNumber,
+                    pageSize: limit,
+                    totalPages: Math.ceil(count / limit),
+                    count,
+                    teams,
+                });
+            } else {
+                const teams = await getAllTeams(options);
+
+                resolve({
+                    count: teams.length,
+                    teams,
+                });
+            }
+        } catch(err) {
+            reject(err);
+        }
+    });
+};
+
+const getEventsWithLimit = async function(limit: number, offset: number, options: object): Promise<Array<models.Event>> {
+    return models.Event.findAll({
+        limit,
+        offset,
+        ...options,
+    });
+};
+
+const getAllEvents = async function(options: object): Promise<Array<models.Event>> {
+    return models.Event.findAll(options);
+};
+
+const getEvents = async function(pageNumber: number = 1, limit: ?number): Promise<Array<models.Event>> {
+    const options = {
+        attributes: {
+            exclude: EVENT_ATTRIBUTE_BLACKLIST,
+        },
+    };
+
+    return new Promise(async (resolve, reject) => {
+        try {
+            if (limit) {
+                const offset = pageNumber < 1 ? 0 : (pageNumber - 1) * limit;            
+                const [events, count] = await Promise.all([
+                    getEventsWithLimit(limit, offset, options),
+                    models.Event.count(),
+                ]);
+
+                resolve({
+                    page: pageNumber,
+                    pageSize: limit,
+                    totalPages: Math.ceil(count / limit),
+                    count,
+                    events,
+                });
+            } else {
+                const events = await getAllEvents(options);
+
+                resolve({
+                    count: events.length,
+                    events,
+                });
+            }
+        } catch(err) {
+            reject(err);
+        }
+    });
+
+};
+
 export default {
-  getUserPage,
+  getUsers,
+  getTeams,
+  getEvents,
 };
--- src/app/routes/public.routes.js
+++ src/app/routes/public.routes.js
@@ -9,17 +9,41 @@ import { BadRequestError } from '../errors';
 export default function(app: express$Application) {
     const publicRouter = express.Router();
 
-    const getUserPage = async (req: $Request, res: $Response) => {
+    const getUsers = async (req: $Request, res: $Response) => {
         let pageNumber = parseInt(req.query.page),
             limit = parseInt(req.query.limit) ? parseInt(req.query.limit) : undefined;
 
         if (!pageNumber || pageNumber < 1) pageNumber = 1;
 
-        const userPage = await publicController.getUserPage(pageNumber, limit);
+        const result = await publicController.getUsers(pageNumber, limit);
 
-        res.json(userPage);
+        res.json(result);
     };
 
-    publicRouter.get('/users', wrap(getUserPage));
+    const getTeams = async (req: $Request, res: $Response) => {
+        let pageNumber = parseInt(req.query.page),
+            limit = parseInt(req.query.limit) ? parseInt(req.query.limit) : undefined;
+
+        if (!pageNumber || pageNumber < 1) pageNumber = 1;
+
+        const result = await publicController.getTeams(pageNumber, limit);
+
+        res.json(result);
+    };
+
+    const getEvents = async (req: $Request, res: $Response) => {
+        let pageNumber = parseInt(req.query.page),
+            limit = parseInt(req.query.limit) ? parseInt(req.query.limit) : undefined;
+
+        if (!pageNumber || pageNumber < 1) pageNumber = 1;
+
+        const result = await publicController.getEvents(pageNumber, limit);
+
+        res.json(result);
+    };
+
+    publicRouter.get('/users', wrap(getUsers));
+    publicRouter.get('/teams', wrap(getTeams));
+    publicRouter.get('/events', wrap(getEvents));
     app.use('/public', publicRouter)
 }
