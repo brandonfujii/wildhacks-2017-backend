36fa075aa59651934caa52d5f5917ea42045f84c
brandonfujii <brandonfujii2018@u.northwestern.edu>
2017-11-03 19:50:07 -0500

    increasing to xlarge size

--- config/index.js
+++ config/index.js
@@ -9,7 +9,7 @@ var config = {
         host: process.env.DATABASE_HOST || '127.0.0.1',
         dialect: 'mysql',
         pool: {
-            max: 25,
+            max: 1000,
             min: 0,
             idle: 10000,
             acquire: 10000
